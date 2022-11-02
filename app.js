0bbb2df7f83838a202476472b208e58af4a5786b
Beny <Beny@Emilys-MacBook-Air.local>
2022-11-02 16:35:19 -0500

    its getting there

---
+++ app.js
@@ -0,0 +1,52 @@
+$(()=> {
+    $('.home').on('click',() => {
+        $(this).toggleClass('.reveal')
+    })
+})
+
+
+$('div').on('click', function() {
+    $(this).toggleClass('show-description');
+})
+
+
+$(() => {
+
+    let currentImgIndex = 0;
+
+    let numOfImages = $('.carousel-images').children().length - 1
+
+    $('.next').on('click', () => {
+        $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+
+        if(currentImgIndex < numOfImages) {
+            currentImgIndex ++
+           } else {
+            currentImgIndex = 0
+           }
+
+        $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+
+
+    })
+
+    $('.previous').on('click', () => {
+        $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+
+        if(currentImgIndex > 0) {
+            currentImgIndex --
+          } else {
+            currentImgIndex = numOfImages
+          }
+          
+
+        $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+        
+       })
+      
+
+
+
+
+
+});
